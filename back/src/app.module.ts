13818bfe52d5a55790ce2f058fde21aac55deebe
Igor Kamyshev <igor@kamyshev.me>
2019-01-28 20:08:21 +0300

    feat(user): add configuration module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 
+import { ConfigModule } from './config/config.module'
 import { MoneyModule } from './money/money.module'
 import { UserModule } from './user/user.module'
 import { UtilsModule } from './utils/utils.module'
 
 @Module({
-  imports: [UserModule, MoneyModule, UtilsModule],
+  imports: [UserModule, MoneyModule, UtilsModule, ConfigModule],
 })
 export class AppModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
