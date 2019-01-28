da98062a37511c6f00602632bdc6b3f4cc6afd69
Igor Kamyshev <igor@kamyshev.me>
2019-01-28 10:23:06 +0300

    fix: fix typo

--- back/src/money/money.module.ts
+++ back/src/money/money.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 
-import { HistoryController } from './presentation/http/controller/HIstoryController'
+import { HistoryController } from './presentation/http/controller/HistoryController'
 import { StatisticsController } from './presentation/http/controller/StatisticsController'
 import { TransactionController } from './presentation/http/controller/TransactionController'
 
