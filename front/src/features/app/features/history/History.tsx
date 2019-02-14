9ac39bd3a58ea0ba69ca4b6066feab18efb062c4
Igor Kamyshev <igor@kamyshev.me>
2019-02-14 12:10:18 +0200

    refactor(money): extract incomes and outcomes

--- front/src/features/app/features/history/History.tsx
+++ front/src/features/app/features/history/History.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useMappedState } from 'redux-react-hook'
-import { Option } from 'tsoption'
 
 import { fetchHistory } from '@front/domain/money/actions/fetchHistory'
 import { getFetchingStatus } from '@front/domain/money/selectors/getFetchingStatus'
@@ -9,32 +8,31 @@ import { getHistory } from '@front/domain/money/selectors/getHistory'
 import { Loader } from '@front/ui/loader'
 import { GroupBy } from '@shared/enum/GroupBy'
 
-import { Groupment } from './Groupment'
-import { Period } from './Period'
+import { Groupment } from './organisms/Groupment'
+import { Incomes } from './organisms/Incomes'
+import { Outcomes } from './organisms/Outcomes'
+import { Period } from './organisms/Period'
 
 export const History = () => {
   const firstTransactionDate = useMappedState(getFirstTransactionDate)
+  const fetching = useMappedState(getFetchingStatus)
+  const dispatch = useDispatch()
 
   const [from, setFrom] = useState(firstTransactionDate)
   const [to, setTo] = useState(new Date())
   const [groupBy, setGroupBy] = useState(GroupBy.Year)
 
-  const fetching = useMappedState(getFetchingStatus)
+  const updateTriggers = [from, to, groupBy]
 
-  const historySelector = useCallback(getHistory(from, to, groupBy), [
-    from,
-    to,
-    groupBy,
-  ])
+  const historySelector = useCallback(
+    getHistory(from, to, groupBy),
+    updateTriggers,
+  )
   const history = useMappedState(historySelector)
 
-  const dispatch = useDispatch()
-  useEffect(
-    () => {
-      dispatch(fetchHistory(from, to, groupBy) as any)
-    },
-    [from, to, groupBy],
-  )
+  useEffect(() => {
+    dispatch(fetchHistory(from, to, groupBy) as any)
+  }, updateTriggers)
 
   return (
     <>
@@ -42,35 +40,14 @@ export const History = () => {
       <Groupment groupBy={groupBy} updateGroupBy={setGroupBy} />
       <Period start={from} updateStart={setFrom} end={to} updateEnd={setTo} />
       <Loader status={fetching}>
-        {history
-          .map(realHistory =>
-            realHistory.map(group => (
-              <>
-                <h3>{group.title}</h3>
-                <h4>Incomes</h4>
-                {group.incomes.map(({ amount, currency, source, date }) => (
-                  <p>
-                    {Option.of(date)
-                      .map(d => d.toDateString())
-                      .getOrElse('')}
-                    {' — '}
-                    {amount / 100} {currency} ({source})
-                  </p>
-                ))}
-                <h4>Outcomes</h4>
-                {group.outcomes.map(({ amount, currency, category, date }) => (
-                  <p>
-                    {Option.of(date)
-                      .map(d => d.toDateString())
-                      .getOrElse('')}
-                    {' — '}
-                    {amount / 100} {currency} ({category})
-                  </p>
-                ))}
-              </>
-            )),
-          )
-          .getOrElse([])}
+        {history.nonEmpty() &&
+          history.get().map(({ title, incomes, outcomes }) => (
+            <>
+              <h3>{title}</h3>
+              <Incomes incomes={incomes} />
+              <Outcomes outcomes={outcomes} />
+            </>
+          ))}
       </Loader>
     </>
   )
---
+++ front/src/features/app/features/history/organisms/Outcomes.tsx
@@ -0,0 +1,22 @@
+import { Option } from 'tsoption'
+
+import { OutcomeModel } from '@shared/models/money/OutcomeModel'
+
+interface Props {
+  outcomes: OutcomeModel[]
+}
+
+export const Outcomes = ({ outcomes }: Props) => (
+  <>
+    <h4>Outcomes</h4>
+    {outcomes.map(({ amount, currency, category, date }) => (
+      <p>
+        {Option.of(date)
+          .map(_ => _.toDateString())
+          .getOrElse('')}
+        {' — '}
+        {amount / 100} {currency} ({category})
+      </p>
+    ))}
+  </>
+)
