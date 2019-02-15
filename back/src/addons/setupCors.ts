87ea5377d21446302519656c095744d3dd4899ac
Igor Kamyshev <igor@kamyshev.me>
2019-02-15 21:38:20 +0200

    fix: fix cors policy

--- back/src/addons/setupCors.ts
+++ back/src/addons/setupCors.ts
@@ -1,27 +1,6 @@
 import { INestApplication } from '@nestjs/common'
 import * as cors from 'cors'
 
-const corsMiddleware = () => {
-  const corsOptions = {
-    origin: '*',
-    credentials: true,
-    allowHeaders: [
-      'DNT',
-      'User-Agent',
-      'X-Requested-With',
-      'If-Modified-Since',
-      'Cache-Control',
-      'Content-Type',
-      'Range',
-      'Authorization',
-      'Cookie',
-    ],
-    methods: ['GET', 'POST', 'OPTIONS'],
-  }
-
-  return cors(corsOptions)
-}
-
 export const setupCors = (app: INestApplication) => {
-  app.use(corsMiddleware())
+  app.use(cors())
 }
