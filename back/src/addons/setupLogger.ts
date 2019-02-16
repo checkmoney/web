ddfb22d80431244a2a7c7c784abf89d1f2e43f71
Igor Kamyshev <igor@kamyshev.me>
2019-02-16 18:36:17 +0200

    feat: add logger

---
+++ back/src/addons/setupLogger.ts
@@ -0,0 +1,6 @@
+import { INestApplication } from '@nestjs/common'
+import * as morgan from 'morgan'
+
+export const setupLogger = (app: INestApplication) => {
+  app.use(morgan('common'))
+}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 
 import { setupCors } from '@back/addons/setupCors'
+import { setupLogger } from '@back/addons/setupLogger'
 import { setupSwagger } from '@back/addons/setupSwagger'
 import { AppModule } from '@back/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  setupSwagger(app, 'docs')
   setupCors(app)
+  setupLogger(app)
+  setupSwagger(app, 'docs')
 
   await app.listen(3000)
 }
