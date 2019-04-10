d6cb2323bd781c4b97e4ad541869da5ee6554706
iwanttobealight <iwanttobealight@yandex.ru>
2019-04-11 00:10:16 +0300

    fix(bar-chart): update statistics colors

--- front/src/ui/components/chart/bar-chart/helpers/getColor.ts
+++ front/src/ui/components/chart/bar-chart/helpers/getColor.ts
@@ -1,6 +1,6 @@
 import Color from 'color'
 
-const colors = ['#00BCD4', '#FFC107', '#673AB7', '#F44336']
+const colors = ['#00D43C', '#FFC107', '#673AB7', '#F44336']
 
 export const getColor = (index: number) =>
   Color(colors[index])
