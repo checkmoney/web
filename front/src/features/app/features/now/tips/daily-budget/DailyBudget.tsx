6951fa6d9b41814b7c554aac6b61d012c8c443b6
uncleseneca <uncleseneca@gmail.com>
2019-04-10 18:24:48 +0300

    refactor(budget tip): remove dismiss button from budget tip

--- front/src/features/app/features/now/tips/daily-budget/DailyBudget.tsx
+++ front/src/features/app/features/now/tips/daily-budget/DailyBudget.tsx
@@ -1,10 +1,6 @@
-import { useCallback } from 'react'
 import { TipModel } from '@shared/models/mind/TipModel'
 import { displayMoney } from '@shared/helpers/displayMoney'
 import { Card } from '@front/ui/components/layout/card'
-import { Button, ButtonType } from '@front/ui/components/form/button'
-import { useThunk } from '@front/domain/store'
-import { disableTips } from '@front/domain/mind/actions/disableTips'
 import { DailyBudgetMeta } from './DailyBudgetMeta'
 import * as styles from '../components/merge/Merge.css'
 
@@ -12,20 +8,10 @@ interface Props {
   tip: TipModel<DailyBudgetMeta>
 }
 
-export const DailyBudget = ({ tip: { token, meta } }: Props) => {
-  const dispatch = useThunk()
-  const onDismiss = useCallback(() => dispatch(disableTips([token])), [token])
-
+export const DailyBudget = ({ tip: { meta } }: Props) => {
   return (
     <div className={styles.card}>
-      <Card
-        title={`Your daily budget`}
-        extra={
-          <Button type={ButtonType.Text} onClick={onDismiss}>
-            Dismiss
-          </Button>
-        }
-      >
+      <Card title={`Your daily budget`}>
         Your preferred budget today is{' '}
         {displayMoney(meta.currency)(meta.amount)}
       </Card>
