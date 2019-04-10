83bda11ceb172dbd83dbc0e6ca472a0efd780bbb
iwanttobealight <iwanttobealight@yandex.ru>
2019-04-10 22:51:13 +0300

    feat(hello): add CurrencySwitch

--- front/src/features/hello/Hello.tsx
+++ front/src/features/hello/Hello.tsx
@@ -5,7 +5,7 @@ import { useCallback } from 'react'
 
 import * as styles from './Hello.css'
 import { setDefaultCurrency } from '@front/domain/user/actions/setDefaultCurrency'
-import { getUserProfile } from '@front/domain/user/actions/getUserProfile'
+import { CurrencySwitch } from '@front/ui/components/controls/currency-switch'
 import { Currency } from '@shared/enum/Currency'
 
 export const Hello = () => {
@@ -16,20 +16,13 @@ export const Hello = () => {
     await dispatch(setDefaultCurrency(currency))
   }, [])
 
-  const getProfile = useCallback(async () => {
-    await dispatch(getUserProfile())
-  }, [])
-
   return (
     <section className={styles.container}>
       <h1>
         Checkmoney Space
         <br />
-        <button onClick={() => getProfile()}>get your profile</button>
-        <button onClick={() => setCurrency(Currency.RUB)}>
-          set your currency
-        </button>
         <small>Wellcome!</small>
+        <CurrencySwitch currency={Currency.USD} updateCurrency={setCurrency} />
       </h1>
       <Button onClick={() => pushRoute('/app')}>Start</Button>
     </section>
