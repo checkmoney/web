95a091c94cd35ff5b37ea41194c84cbe2e782020
Igor Kamyshev <igor@kamyshev.me>
2019-04-15 18:56:02 +0300

    feat(currency): change column type to var char

--- back/evolutions/7.sql
+++ back/evolutions/7.sql
@@ -1,5 +1,26 @@
-ALTER TYPE public.currency ADD VALUE 'SEK';
-ALTER TYPE public.currency ADD VALUE 'NOK';
-ALTER TYPE public.currency ADD VALUE 'DKK';
+ALTER TABLE public.exchange_rate
+  ALTER COLUMN "from" TYPE character varying,
+  ALTER COLUMN "to" TYPE character varying;
+
+ALTER TABLE public.income ALTER COLUMN currency TYPE character varying;
+ALTER TABLE public.outcome ALTER COLUMN currency TYPE character varying;
+
+ALTER TABLE public."user" ALTER COLUMN "profile_defaultcurrency" TYPE character varying;
+
+DROP TYPE public.currency;
 
 #DOWN
+
+CREATE TYPE public.currency AS ENUM (
+  'RUB',
+  'USD',
+  'EUR'
+);
+
+ALTER TABLE public.exchange_rate ALTER COLUMN "from" TYPE public.currency USING "from"::text::public.currency;
+ALTER TABLE public.exchange_rate ALTER COLUMN "to" TYPE public.currency USING "to"::text::public.currency;
+
+ALTER TABLE public.income ALTER COLUMN currency TYPE public.currency USING currency::text::public.currency;
+ALTER TABLE public.outcome ALTER COLUMN currency TYPE public.currency USING currency::text::public.currency;
+
+ALTER TABLE public."user" ALTER COLUMN "profile_defaultcurrency" TYPE public.currency USING "profile_defaultcurrency"::text::public.currency;
