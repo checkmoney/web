81729967e25e1321e62d2f9cdeebd0a01d29e332
Igor Kamyshev <igor@kamyshev.me>
2019-04-18 15:33:27 +0300

    feat(user): add sign-out button

---
+++ front/src/domain/user/actions/signOut.ts
@@ -0,0 +1,13 @@
+import { fetchOrFail } from '@front/domain/store'
+
+import { actions as dataActions } from '../reducer/data'
+import { resetCookie } from '../helpers/resetCookie'
+
+const { resetToken } = dataActions
+
+export const signOut = () =>
+  fetchOrFail(undefined, async dispatch => {
+    resetCookie()
+
+    dispatch(resetToken())
+  })
