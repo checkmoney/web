41e72c0495cb9bbfecc22e5e43a1bf11fce3ba85
Igor Kamyshev <igor@kamyshev.me>
2019-05-11 21:17:19 +0300

    feat(telegram): add ability to send messages to telegram

--- back/src/mind/mind.module.ts
+++ back/src/mind/mind.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { ModuleRef } from '@nestjs/core'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { TelegramModule } from '@back/telegram/telegram.module'
 import { MoneyModule } from '@back/money/money.module'
 import { UserModule } from '@back/user/user.module'
 import { DbModule } from '@back/db/db.module'
@@ -31,6 +32,7 @@ import { RecurrentPaymentAdviser } from './application/adviser/RecurrentPaymentA
     MoneyModule,
     TypeOrmModule.forFeature([DisabledTip]),
     TypeOrmModule.forFeature([CustomTip]),
+    TelegramModule,
   ],
   controllers: [TipController, TypoController],
   providers: [
