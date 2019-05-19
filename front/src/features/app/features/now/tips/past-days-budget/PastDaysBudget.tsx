d346281ecfd3df9d7659291d96347092e587192b
Igor Kamyshev <igor@kamyshev.me>
2019-05-19 21:56:13 +0300

    feat(week): add past days budhet on front

---
+++ front/src/features/app/features/now/tips/past-days-budget/PastDaysBudget.tsx
@@ -0,0 +1,29 @@
+import { TipModel } from '@shared/models/mind/TipModel'
+
+import { displayMoney } from '@shared/helpers/displayMoney'
+import { Card } from '@front/ui/components/layout/card'
+import { useTranslation } from '@front/domain/i18n'
+
+import { DismissButton } from '../components/dismiss-button'
+import { PastDaysBudgetMeta } from './PastDaysBudgetMeta'
+
+interface Props {
+  tip: TipModel<PastDaysBudgetMeta>
+}
+
+export const PastDaysBudget = ({ tip: { token, meta } }: Props) => {
+  const { t } = useTranslation()
+
+  const amount = displayMoney(meta.currency)(meta.outcome, {
+    withPenny: false,
+  })
+
+  return (
+    <Card
+      title={t(`tips:past-days.title-${meta.group}`)}
+      extra={<DismissButton token={token} />}
+    >
+      {t(`tips:past-days.content-${meta.group}`, { amount })}
+    </Card>
+  )
+}
