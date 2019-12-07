8f24b498514ccf01e472d1d29d0f0230890477e0
Igor Kamyshev <igor@kamyshev.me>
2019-12-07 23:46:04 +0300

    refactor: move landing to separate repo

--- landing/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-custom-media')({
-      importFrom: './src/custom-media.css',
-    }),
-  ],
-};
--- landing/src/interactive/log.ts
+++
@@ -1,5 +0,0 @@
-import { lol } from '&landing/lol';
-import { formatDate } from '&shared/helpers/formatDate';
-
-console.log(lol());
-console.log(formatDate(new Date()), lol());
