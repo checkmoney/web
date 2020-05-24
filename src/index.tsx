7954c60c8715d791df8b0d72b0ecf65167cf7a69
Igor Kamyshev <igor@kamyshev.me>
2020-05-24 08:39:43 +0700

    fix: add head

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,6 @@ import { initializeStore } from './domain/store/initializeStore';
 const reduxStore = initializeStore();
 
 function Root() {
-  // {/* <Head>
-  //     <script src="//unpkg.com/reflect-metadata@0.1.13/Reflect.js" />
-  //     <meta name="viewport" content="width=device-width, initial=scale=1" />
-  // </Head> */}
   return (
     <Provider store={reduxStore}>
       <RouterProvider router={router}>
